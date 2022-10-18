5f16275650367fe6d571860b5775d7f4843a4b11
Simon <113917705+cossinsim@users.noreply.github.com>
2022-10-18 23:14:17 +0200

    Utilisation d'un fichier contenant  data nécessaire (tableau d'objets) à la génération de nouveau composants
    <Card /> maintenant générées en fonction des objets contenus dans un tableau d'objets (data.js). Utilisation de map() pour générer nouvelle <Card /> avec props. correspondantes pour chaque objet contenu dans ce tableau.

--- 2_Projet_Data_Driven_React/src/components/App.jsx
+++ 2_Projet_Data_Driven_React/src/components/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Navbar from "./Navbar.jsx"
 import Hero from './Hero.jsx'
 import Card from './Card.jsx'
+import data from '../data/data.js'
+
 import './App.css'
 
 function App() {
@@ -10,7 +12,24 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <section className="courses">
+      {data.map(experience => {
+        return (
+          <Card
+          id={experience.id}
+          title={experience.title}
+          description={experience.description}
+          price={experience.price}
+          coverImg={experience.coverImg}
+          imgAlt={experience.imgAlt}
+          rating={experience.stats.rating}
+          reviewCount={experience.stats.reviewCount}
+          location={experience.location}
+          />
+        )
+      }
+      )}
+      </section>
     </div>
   )
 
--- 2_Projet_Data_Driven_React/src/components/Card.jsx
+++ 2_Projet_Data_Driven_React/src/components/Card.jsx
@@ -3,42 +3,24 @@ import './Card.css'
 import katie from "../assets/Cards/katie-zaferes.png"
 import star from "../assets/Cards/star.png"
 
-function Card() {
+function Card(props) {
     return (
-        <section className="courses">
-            <div className="card">
-                <img className="katies-img" src={katie} alt="Katie Zaferes" />
+            <div className="card" id={props.id}>
+                <img className="card-img" src={`/src/assets/Cards/${props.coverImg}`} alt={props.imageAlt} />
                 <article className="lesson">
                     <div className="lesson-infos">
                         <img className="lesson-infos-star" src={star} alt="star" />
-                        <span className="lesson-rating">5.0 </span>
-                        <span className="total-lesson-ratings">(6)</span>
-                        <span className="teacher-location">USA</span>
+                        <span className="lesson-rating">{props.rating.toFixed(1)}</span>
+                        <span className="total-lesson-ratings">({props.reviewCount})</span>
+                        <span className="teacher-location">{props.location}</span> 
                     </div>
-                    <p className="lesson-description">Life lessons with Katie Zaferes</p>
+                    <p className="lesson-description">{props.title}</p>
                     <p className="pricing">
-                        <span className="from-price">From $136</span>
+                        <span className="from-price">${props.price}</span>
                         <span> / person</span>
                     </p>
                 </article>
             </div>
-            <div className="card">
-                <img className="katies-img" src={katie} alt="Katie Zaferes" />
-                <article className="lesson">
-                    <div className="lesson-infos">
-                        <img className="lesson-infos-star" src={star} alt="star" />
-                        <span className="lesson-rating">5.0 </span>
-                        <span className="total-lesson-ratings">(6)</span>
-                        <span className="teacher-location">USA</span>
-                    </div>
-                    <p className="lesson-description">Life lessons with Katie Zaferes</p>
-                    <p className="pricing">
-                        <span className="from-price">From $136</span>
-                        <span> / person</span>
-                    </p>
-                </article>
-            </div>
-        </section>   
     )
 }
 
